2a227782da9e8f48f94821a1463fb5e344505ec3
amr <amr.hazem@seal-software.com>
2020-02-28 20:37:28 +0200

    index. js first trial for graphQl

--- src/index.js
+++ src/index.js
@@ -2,11 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { gql } from 'apollo-boost';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import store from './store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+	uri: 'https://api-euwest.graphcms.com/v1/ck75bq3rr0k5i01cyf9p599sh/master',
+});
+
+client
+	.mutate({
+		mutation: gql`
+			mutation {
+				updateTask(
+					data: { title: "RubixTask", description: "you have an obligation to end the rubix task" }
+					where: { id: "ck7699nkdez4g0b20kczng64q" }
+				) {
+					id
+				}
+			}
+		`,
+	})
+	.then(result => console.log(result));
+
+client
+	.query({
+		query: gql`
+			{
+				tasks
+			}
+		`,
+	})
+	.then(result => console.log(result));
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
