1995846edf2d755585ec9a055ee0f7715e07edac
amr <amr.hazem@seal-software.com>
2020-03-04 17:12:34 +0200

    tasks actions manifesting create update delete and completed import

--- src/actions/tasksActions.js
+++ src/actions/tasksActions.js
@@ -3,23 +3,138 @@ import { API_SERVICE } from '../service/apiService';
 import { TASK } from './types';
 import store from '../store';
 
-export function importAllTasks() {
+export function importAllTasks(input) {
 	store.dispatch({ type: TASK.IMPORT_ALL_REQUEST });
 	return API_SERVICE.MYTASK_API.query({
 		query: gql`
-			{
-				tasks {
+			query test($input: Int = ${input}) {
+				tasks(first: 5, skip: $input) {
 					id
 					title
+					description 
+					completed
+                    dueTime
+				}
+               
+			}
+            
+		`,
+	})
+		.then(result => {
+			return store.dispatch({ type: TASK.IMPORT_ALL_SUCCESS, payload: result.data.tasks });
+		})
+		.catch(err => {
+			console.log(err);
+			store.dispatch({ type: TASK.IMPORT_ALL_FAILED });
+		});
+}
+
+export function importAllCompletedTasks(input) {
+	store.dispatch({ type: TASK.IMPORT_ALL_COMPLETED_REQUEST });
+	return API_SERVICE.MYTASK_API.query({
+		query: gql`
+			query test($input: Int = ${input}) {
+				tasks(first: 10, skip: $input, where: { completed: true }) {
+					id
+					title
+					description
+					completed
+					dueTime
+				}
+                tasksConnection( where:{completed:true}) {
+     		    aggregate{
+                count
+                    }
+                 }
+
+			}
+		`,
+	})
+		.then(result => {
+			console.log('HERE', result);
+			return store.dispatch({
+				type: TASK.IMPORT_ALL_COMPLETED_SUCCESS,
+				payload: result.data,
+			});
+		})
+		.catch(err => {
+			console.log(err);
+			store.dispatch({ type: TASK.IMPORT_ALL_COMPLETED_FAILED, payload: err });
+		});
+}
+
+export function createTask(input) {
+	store.dispatch({ type: TASK.CREATE_REQUEST });
+
+	return API_SERVICE.MYTASK_API.mutate({
+		variables: { input },
+		mutation: gql`
+			mutation createTask($input: TaskCreateInput!) {
+				createTask(data: $input) {
+					id
+					title
+					completed
 				}
 			}
 		`,
 	})
 		.then(result => {
-			return store.dispatch({ type: TASK.IMPORT_ALL_SUCCESS, payload: result });
+			console.log('RES', result);
+			return store.dispatch({ type: TASK.IMPORT_ALL_SUCCESS, payload: result.data.tasks });
 		})
 		.catch(err => {
-			console.log(err);
+			console.log('err', err);
 			store.dispatch({ type: TASK.IMPORT_ALL_FAILED });
+		});
+}
+
+export function deleteTask(input) {
+	store.dispatch({ type: TASK.DELETE_REQUEST });
+
+	return API_SERVICE.MYTASK_API.mutate({
+		variables: { input },
+		mutation: gql`
+			mutation deleteTask($input: ID!) {
+				deleteTask(where: { id: $input }) {
+					id
+					title
+					completed
+				}
+			}
+		`,
+	})
+		.then(result => {
+			console.log('RES', result);
+			return store.dispatch({ type: TASK.DELETE_SUCCESS, payload: result.data.tasks });
+		})
+		.catch(err => {
+			console.log('err', err);
+			store.dispatch({ type: TASK.DELETE_FAILURE, payload: err });
+		});
+}
+
+export function updateTask(input) {
+	store.dispatch({ type: TASK.DELETE_REQUEST });
+	const { id } = input;
+	delete input.id;
+	return API_SERVICE.MYTASK_API.mutate({
+		variables: { input, id },
+		mutation: gql`
+			mutation updateTask($input: TaskUpdateInput!, $id: ID!) {
+				updateTask(data: $input, where: { id: $id }) {
+					id
+					title
+					completed
+				}
+			}
+		`,
+	})
+		.then(result => {
+			console.log('RES', result);
+			return store.dispatch({ type: TASK.UPDATE_SUCCESS, payload: result.data.tasks });
+		})
+		.catch(err => {
+			console.log('err', err);
+			store.dispatch({ type: TASK.UPDATE_FAILED, payload: err });
 		});
 }
