d695292cded2cf6ea9319cfb6a64c16fbba82fc3
amr <amr.hazem@seal-software.com>
2020-03-04 18:14:21 +0200

    reducer addition to setup

--- src/reducers/tasksReducer.js
+++ src/reducers/tasksReducer.js
@@ -1,20 +1,29 @@
 import { TASK } from '../actions/types';
 
 const initialState = {
-	searchTasksResults: [],
-	totalSearchResults: 0,
-	currentSearchPageOffset: 0,
-	searchResultsPageSize: 20,
+	tasksResults: [],
+	completedTasks: [],
+	pageSize: 10,
+	completedTasksCount: 0,
 	searchLoading: false,
 	createLoading: false,
 	deleteLoading: false,
 };
 
-export default function navigateToInATNbodyReducer(state = initialState, action) {
+export default function tasksReducer(state = initialState, action) {
 	switch (action.type) {
 		case TASK.IMPORT_ALL_SUCCESS:
+			const { tasksResults } = state;
 			return {
 				...state,
+				tasksResults: [...tasksResults, ...action.payload],
+			};
+
+		case TASK.IMPORT_ALL_COMPLETED_SUCCESS:
+			return {
+				...state,
+				completedTasks: [...action.payload.tasks],
+				completedTasksCount: action.payload.tasksConnection.aggregate.count,
 			};
 
 		default:
