3a2d05ffe02abc9d36d91b2e647d61f27312d66e
amr <amr.hazem@seal-software.com>
2020-03-04 20:15:29 +0200

    List containers done

--- src/components/BaseLayout/TasksComponents/CompletedListContainer.js
+++ src/components/BaseLayout/TasksComponents/CompletedListContainer.js
@@ -1,7 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { connect } from 'react-redux';
+import { importAllCompletedTasks } from '../../../actions/tasksActions';
+import { Pagination } from 'semantic-ui-react';
 
-const CompletedListContainer = () => {
-	return <div>CompletedListContainer</div>;
+import TasksList from './components/TasksLists';
+
+const CompletedListContainerWrapper = styled.div`
+	background: #8080805c;
+	width: 100%;
+	padding: 25px;
+	padding-bottom: 80px;
+	margin: 0 20px;
+	height: fit-content;
+	display: flex;
+	flex-direction: column;
+	.ui.pagination.menu {
+		width: fit-content;
+		margin: auto;
+	}
+`;
+const CompletedListContainer = props => {
+	const { tasksReducer } = props;
+	const { completedTasks, completedTasksCount, pageSize } = tasksReducer;
+
+	console.log('completedTasks', completedTasks);
+	useEffect(() => {
+		importAllCompletedTasks(0);
+	}, []);
+	const totalPages = Math.ceil(completedTasksCount / pageSize);
+	console.log(totalPages);
+
+	const pageChangeHandler = (_e, data) => {
+		const { activePage } = data;
+		const skipItems = activePage === 1 ? 10 : (activePage - 1) * pageSize;
+		importAllCompletedTasks(skipItems);
+	};
+	return (
+		<CompletedListContainerWrapper>
+			<TasksList tasksResults={completedTasks}></TasksList>
+			<Pagination
+				defaultActivePage={1}
+				onPageChange={(e, data) => pageChangeHandler(e, data)}
+				totalPages={totalPages}
+				siblingRange={3}
+			/>
+		</CompletedListContainerWrapper>
+	);
 };
 
-export default CompletedListContainer;
+const mapStateToProps = ({ tasksReducer }) => ({
+	tasksReducer,
+});
+
+export default connect(mapStateToProps, { importAllCompletedTasks })(CompletedListContainer);
--- src/components/BaseLayout/TasksComponents/TasksListContainer.js
+++ src/components/BaseLayout/TasksComponents/TasksListContainer.js
@@ -1,7 +1,70 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { Visibility } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import { importAllTasks, createTask, deleteTask, updateTask } from '../../../actions/tasksActions';
+import { Loader } from 'semantic-ui-react';
 
-const TasksListContainer = () => {
-	return <div>taskssListContainer</div>;
+import TasksList from './components/TasksLists';
+
+const TasksListContainerWrapper = styled.div`
+	background: #8080805c;
+	width: 100%;
+	padding: 100px;
+	margin: 0 20px;
+	height: fit-content;
+	.ui.inverted.loader {
+		position: fixed;
+		top: 85%;
+		font-size: 15px;
+	}
+`;
+
+const TasksListContainer = props => {
+	const { tasksReducer } = props;
+	const { tasksResults } = tasksReducer;
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		importAllTasks(0);
+		// const task = { title: 'NewTask1', description: 'syuhdysgdysg', completed: true };
+		// createTask();
+		// deleteTask('ck79ez9tqauee0b84u341zr53');
+		// const task = {
+		// 	id: 'ck77r3069hl4x0b2019dy3x43',
+		// 	title: 'newYess',
+		// 	completed: true,
+		// 	description: 'describe yes ofcourse',
+		// };
+		// updateTask(task);
+	}, []);
+
+	useEffect(() => {
+		setLoading(false);
+	}, [tasksResults]);
+
+	const VisibilityHandler = (_e, data) => {
+		const { calculations } = data;
+		const { bottomVisible, bottomPassed } = calculations;
+		console.log(bottomVisible, bottomPassed);
+		if (bottomVisible)
+			if (!loading) {
+				setLoading(true);
+				importAllTasks(tasksResults.length);
+			}
+	};
+	return (
+		<TasksListContainerWrapper>
+			<Loader inverted active={loading} content="Loading Items Please wait..." />
+			<Visibility onUpdate={(e, calculations) => VisibilityHandler(e, calculations)}>
+				<TasksList {...tasksReducer} />
+			</Visibility>
+		</TasksListContainerWrapper>
+	);
 };
 
-export default TasksListContainer;
+const mapStateToProps = ({ tasksReducer }) => ({
+	tasksReducer,
+});
+
+export default connect(mapStateToProps, { importAllTasks, createTask, deleteTask, updateTask })(TasksListContainer);
