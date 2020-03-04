0266ec7394db5c139b502ecf9c9217ee02d69505
amr <amr.hazem@seal-software.com>
2020-03-04 20:30:53 +0200

    indexes fix

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -1,5 +1,6 @@
 import Footer from './Footer';
 import Header from './Header';
 import Body from './Body';
+import ModalComponent from './Modal';
 
-export { Footer, Header, Body };
+export { Footer, Header, Body, ModalComponent };
